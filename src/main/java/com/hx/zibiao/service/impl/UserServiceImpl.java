c6cac51fed6a67e08f1ae21dac43cf1d60d50535
李依若 <401213245@qq.com>
2019-09-16 15:37:44 +0800

    修改注释，调整了格式

--- src/main/java/com/hx/zibiao/service/impl/UserServiceImpl.java
+++ src/main/java/com/hx/zibiao/service/impl/UserServiceImpl.java
@@ -29,15 +29,13 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public List<User> testT() {
-//添加了事务 并且生效了
+        //添加了事务 并且生效了
         User user = new User();
         user.setId(16);
         user.setName("第十6");
         userDao.addUser(user);
         //如果注释掉事务注解 则会添加成功一条
         userDao.addUser(user);
-
-
         return userDao.getUsers();
     }
 }
